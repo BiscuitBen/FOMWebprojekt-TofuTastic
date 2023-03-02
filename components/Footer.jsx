83c9647721ae7e4139359e2d6e998d5678c25fca
BiscuitBen <benedictseidel@outlook.de>
2023-03-02 07:50:43 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+    <div className='bg-white border-red-700 border-t-2 border-b-2 text-center font-semibold fixed bottom-0 left-0 right-0'>
+      Disclaimer: Diese Webanwendung wurde im Rahmen eines Hochschulprojektes in dem Modul Webtechnologien erstellt und wird derzeit nur für diesen Zweck verwendet.   
+    </div>
+  )
+}
+
+export default Footer
